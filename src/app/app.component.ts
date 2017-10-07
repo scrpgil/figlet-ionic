a8ea03e23d0e2a4f78a925782a76f60c4d83353e
scrpgil <scraplbuild@gmail.com>
2017-10-07 12:41:04 +0900

    アナリティクス追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,20 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(private ga: GoogleAnalytics, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      
+      this.ga.startTrackerWithId('UA-84483923-4')
+      .then(() => {
+          this.ga.trackView('top');
+          // Tracker is ready
+          // You can now track pages or set additional information such as AppVersion or UserId
+      })
+      .catch(e => console.log('Error starting GoogleAnalytics', e));
     });
   }
 }
