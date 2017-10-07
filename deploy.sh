1fb5c7e01a0bbf126bcb2944b1c9e8c8e5f910b9
scrpgil <scraplbuild@gmail.com>
2017-10-07 12:24:12 +0900

    いろいろ追加

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+rm -rf deploy/browser
+mkdir deploy/browser
+ionic cordova build browser --prod
+cp -rf deploy/lab deploy/browser/
+cp -rf platforms/browser/www/*  deploy/browser/
+sed -i -e "/REDIRECT_SCRIPT/r deploy/redirect.txt" deploy/browser/index.html
+cd deploy/browser
+git init
+git add . 
+git commit -m "auto deploy" 
+git branch gh-pages 
+git checkout gh-pages 
+git remote add origin https://github.com/scrpgil/figlet-ionic.git
+git push -f origin gh-pages
